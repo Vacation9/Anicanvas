832d0af9278fb8264050989f5226a672dcf748ea
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-16 13:57:30 -0400

    beautify code

--- anicanvas.js
+++ anicanvas.js
@@ -7,97 +7,105 @@ function Anicanvas(canvas) {
     this.context = this.canvasObject.getContext('2d'); //this is what we'll be using most often
     this.carray = []; //make array of shapes + text for easy redrawing
     this.addShape = function (properties) {
-        this.carray.push({id:properties.id, shape: properties.shape, props: properties});
-        AniHandler.draw({id:properties.id, shape: properties.shape, props: properties}, this.context, this.canvasObject); //Don't need to redraw when adding shape
+        this.carray.push({
+            id: properties.id,
+            shape: properties.shape,
+            props: properties
+        });
+        AniHandler.draw({
+            id: properties.id,
+            shape: properties.shape,
+            props: properties
+        }, this.context, this.canvasObject); //Don't need to redraw when adding shape
     }
     this.moveShape = function (id, x, y) {
-        var s = AniHandler.findById(this.carray,id);
+        var s = AniHandler.findById(this.carray, id);
         console.log(s);
-        if (typeof(x) == "string" && x.charAt(0) == "+") s.props.x = s.props.x+parseInt(x.substring(1));
-        else if (typeof(x) == "string" && x.charAt(0) == "-") s.props.x = s.props.x-parseInt(x.substring(1));
+        if (typeof (x) == "string" && x.charAt(0) == "+") s.props.x = s.props.x + parseInt(x.substring(1));
+        else if (typeof (x) == "string" && x.charAt(0) == "-") s.props.x = s.props.x - parseInt(x.substring(1));
         else s.props.x = x;
-        if (typeof(y) == "string" && y.charAt(0) == "+") s.props.y = s.props.y+parseInt(y.substring(1));
-        else if (typeof(y) == "string" && y.charAt(0) == "-") s.props.y = s.props.y-parseInt(y.substring(1));
+        if (typeof (y) == "string" && y.charAt(0) == "+") s.props.y = s.props.y + parseInt(y.substring(1));
+        else if (typeof (y) == "string" && y.charAt(0) == "-") s.props.y = s.props.y - parseInt(y.substring(1));
         else s.props.y = y;
         AniHandler.redraw(this.context, this.carray, this.canvasObject); //then redraw; simple!
     }
     this.animateShape = function (id, x, y, ms) {
-        ms = ms/2; //To account for the time it takes to redraw, should implement better fix in the future
-        var s = AniHandler.findById(this.carray,id);
-        AniHandler.animate(id, x, y, (s.props.x-x)/ms, (s.props.y-y)/ms, s.props.x, s.props.y, this);
+        ms = ms / 2; //To account for the time it takes to redraw, should implement better fix in the future
+        var s = AniHandler.findById(this.carray, id);
+        AniHandler.animate(id, x, y, (s.props.x - x) / ms, (s.props.y - y) / ms, s.props.x, s.props.y, this);
     }
 }
 
 //Anicanvas handler definition (not intended to be used publicly)
-var AniHandler =
-{
-    animate: function(id, x, y, speedx, speedy, curx, cury, cobject) {
-        if (Math.abs(curx-x)<0.5 && Math.abs(cury-y)<0.5) return;
+var AniHandler = {
+    animate: function (id, x, y, speedx, speedy, curx, cury, cobject) {
+        if (Math.abs(curx - x) < 0.5 && Math.abs(cury - y) < 0.5) return;
         cobject.moveShape(id, curx, cury);
-        setTimeout(function(){AniHandler.animate(id, x, y, speedx, speedy, curx-speedx, cury-speedy, cobject)}, 1);
+        setTimeout(function () {
+            AniHandler.animate(id, x, y, speedx, speedy, curx - speedx, cury - speedy, cobject)
+        }, 1);
     },
-    findById: function(carray, id) {
+    findById: function (carray, id) {
         for (var i = 0; i < carray.length; i++) {
             if (carray[i].id === id) {
                 return carray[i];
             }
         }
     },
-    draw: function(shape, canvas, canvasObject) {
-        var sv = function(value, defaultValue) {
-            return typeof(value) != 'undefined' ? value : defaultValue; //Bit of hacking around to return x if defined, else a default value
+    draw: function (shape, canvas, canvasObject) {
+        var sv = function (value, defaultValue) {
+            return typeof (value) != 'undefined' ? value : defaultValue; //Bit of hacking around to return x if defined, else a default value
         }
         var defaults = {
-            arc: function(sAngle, eAngle){
-                    canvas.beginPath();
-                    canvas.arc(sv(shape.props.x,canvasObject.width/2), sv(shape.props.y,canvasObject.height/2), sv(shape.props.radius,10), sv(shape.props.sAngle,sAngle), sv(shape.props.eAngle,eAngle), sv(shape.props.counterclockwise,false));
-                    var fillStyle = canvas.fillStyle;
-                    var strokeStyle = canvas.strokeStyle;
-                    var lineWidth = canvas.lineWidth;
-                    canvas.fillStyle = shape.props.color || fillStyle;
-                    canvas.fill();
-                    canvas.lineWidth = sv(shape.props.strokeWidth, lineWidth);
-                    canvas.strokeStyle = shape.props.stroke || strokeStyle;
-                    canvas.stroke();
-                    canvas.fillStyle = fillStyle;
-                    canvas.strokeStyle = strokeStyle;
-                    canvas.lineWidth = lineWidth;
-                },
-            rectangle: function(width, height){
-                    canvas.beginPath();
-                    canvas.rect(sv(shape.props.x,canvasObject.width/2), sv(shape.props.y,canvasObject.height/2), sv(shape.props.width,width), sv(shape.props.height,height));
-                    var fillStyle = canvas.fillStyle;
-                    var strokeStyle = canvas.strokeStyle;
-                    var lineWidth = canvas.lineWidth;
-                    canvas.fillStyle = shape.props.color || fillStyle;
-                    canvas.fill();
-                    canvas.lineWidth = sv(shape.props.strokeWidth, lineWidth);
-                    canvas.strokeStyle = shape.props.stroke || strokeStyle;
-                    canvas.stroke();
-                    canvas.fillStyle = fillStyle;
-                    canvas.strokeStyle = strokeStyle;
-                    canvas.lineWidth = lineWidth;
-                }
+            arc: function (sAngle, eAngle) {
+                canvas.beginPath();
+                canvas.arc(sv(shape.props.x, canvasObject.width / 2), sv(shape.props.y, canvasObject.height / 2), sv(shape.props.radius, 10), sv(shape.props.sAngle, sAngle), sv(shape.props.eAngle, eAngle), sv(shape.props.counterclockwise, false));
+                var fillStyle = canvas.fillStyle;
+                var strokeStyle = canvas.strokeStyle;
+                var lineWidth = canvas.lineWidth;
+                canvas.fillStyle = shape.props.color || fillStyle;
+                canvas.fill();
+                canvas.lineWidth = sv(shape.props.strokeWidth, lineWidth);
+                canvas.strokeStyle = shape.props.stroke || strokeStyle;
+                canvas.stroke();
+                canvas.fillStyle = fillStyle;
+                canvas.strokeStyle = strokeStyle;
+                canvas.lineWidth = lineWidth;
+            },
+            rectangle: function (width, height) {
+                canvas.beginPath();
+                canvas.rect(sv(shape.props.x, canvasObject.width / 2), sv(shape.props.y, canvasObject.height / 2), sv(shape.props.width, width), sv(shape.props.height, height));
+                var fillStyle = canvas.fillStyle;
+                var strokeStyle = canvas.strokeStyle;
+                var lineWidth = canvas.lineWidth;
+                canvas.fillStyle = shape.props.color || fillStyle;
+                canvas.fill();
+                canvas.lineWidth = sv(shape.props.strokeWidth, lineWidth);
+                canvas.strokeStyle = shape.props.stroke || strokeStyle;
+                canvas.stroke();
+                canvas.fillStyle = fillStyle;
+                canvas.strokeStyle = strokeStyle;
+                canvas.lineWidth = lineWidth;
+            }
         }
-        switch (shape["shape"])
-        {
-            case "circle":
-                defaults.arc(0,2 * Math.PI);
-                break;
-            case "semicircle":
-                defaults.arc(0,Math.PI);
-                break;
-            case "arc":
-                defaults.arc(0,Math.PI/2);
-                break;
-            case "rectangle":
-                defaults.rectangle(20, 10);
-                break;
+        switch (shape["shape"]) {
+        case "circle":
+            defaults.arc(0, 2 * Math.PI);
+            break;
+        case "semicircle":
+            defaults.arc(0, Math.PI);
+            break;
+        case "arc":
+            defaults.arc(0, Math.PI / 2);
+            break;
+        case "rectangle":
+            defaults.rectangle(20, 10);
+            break;
         }
     },
-    redraw: function(canvas, carray, canvasObject) {
+    redraw: function (canvas, carray, canvasObject) {
         canvas.clearRect(0, 0, canvasObject.width, canvasObject.height); //clear canvas - so attributes aren't lost, use this method instead of the simpler canvas.width = canvas.width
-        for (var i=0; i<carray.length; i++) {
+        for (var i = 0; i < carray.length; i++) {
             AniHandler.draw(carray[i], canvas, canvasObject); //for each shape redraw the shape
         }
     }
